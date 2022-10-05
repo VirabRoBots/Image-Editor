eae3d9ce3d59dc7349294367bf2656f657b244b2
Virab_RoBots <114412453+VirabRoBots@users.noreply.github.com>
2022-10-05 08:34:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,13 +12,7 @@ For more click help....</b>"""
 <code>🌀 Select the Corresponding mode that you need</code>
 
 <code>🌀 Your Edited Image will be Uploaded </code>
-
-
-© @TroJanzHEX"""
-    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
-
+"""
+    ABOUT_MSG = """⭕️<b>My Name : {}</b>
 ⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
-
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>"""
